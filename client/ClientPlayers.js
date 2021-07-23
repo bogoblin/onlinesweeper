6af05a404a9a0b69228492f2e47afe5ab7171c63
bogoblin <bogoblin@gmail.com>
2021-07-23 21:07:39 +0100

    Show other player's cursors as cursors

--- client/ClientPlayers.js
+++ client/ClientPlayers.js
@@ -1,6 +1,10 @@
 import {Player} from "../shared/Player.js";
 import {PlayerInfo} from "./canvasUI/PlayerInfo.js";
-import {vectorAdd} from "../shared/Vector2.js";
+import {vectorAdd, vectorTimesScalar} from "../shared/Vector2.js";
+
+import cursorUrl from './cursor.png';
+const cursor = new Image();
+cursor.src = cursorUrl;
 
 const TWO_PI = 2*Math.PI;
 
@@ -53,18 +57,26 @@ export class ClientPlayers {
 
         // draw cursors
         for (let player of Object.values(this.players)) {
+            if (player === this.me()) {
+                continue;
+            }
             if (!player.lastClick) {
                 continue;
             }
-            const [x, y] = tileView.worldToScreen(vectorAdd(player.lastClick, [0.5, 0.5]));
+            player.cursorTarget = vectorAdd(player.lastClick, [0.5, 0.5]);
+            if (!player.cursorDisplay) {
+                player.cursorDisplay = player.cursorTarget;
+            }
+            player.cursorDisplay = (vectorTimesScalar(vectorAdd(player.cursorDisplay, player.cursorTarget), 1/2));
+            const [x, y] = tileView.worldToScreen(player.cursorDisplay);
 
-            // todo: make it a cursor
-            context.fillStyle = 'blue';
-            context.beginPath()
-            context.arc(x, y, 4, 0, TWO_PI);
-            context.fill();
+            context.drawImage(cursor, x, y);
             context.font = `20px monospace`;
-            context.fillText(player.username, x+5, y-10);
+            context.strokeStyle = 'white';
+            context.lineWidth = 5;
+            context.strokeText(player.username, x+5, y-5);
+            context.fillStyle = 'blue';
+            context.fillText(player.username, x+5, y-5);
         }
 
         // if you are dead, make the screen red and show the respawn time
